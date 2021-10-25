fe576f106eadb794860902c68885475a2d5a113c
Hariank Muthakana <hariank3k@gmail.com>
2021-10-25 01:07:38 -0400

    registering tasks and checking run schedule

--- programs/solcron/src/lib.rs
+++ programs/solcron/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::clock::UnixTimestamp;
+use anchor_lang::solana_program::clock::Clock;
 use std::convert::Into;
 
 declare_id!("AMPayzfbfW5SqMFCTGrR3x8q5nDv9G3TA9JCQtmSysx1");
@@ -13,62 +13,104 @@ pub trait Scheduled<'info, T: Accounts<'info>> {
 pub mod solcron {
     use super::*;
 
-    // pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn register_task(
+        ctx: Context<RegisterTask>,
+        bump: u8,
+        task_program: Pubkey,
+        start_ts: i64,
+        interval_ts: i64,
+    ) -> ProgramResult {
+        ctx.accounts.task_specification.task_program = task_program;
+        // TODO: validate timestamps
+        ctx.accounts.task_specification.start_ts = start_ts;
+        ctx.accounts.task_specification.interval_ts = interval_ts;
+        ctx.accounts.task_specification.bump = bump;
+        Ok(())
+    }
+
+    // pub fn deregister_task(ctx: Context<DeregisterTask>) -> ProgramResult {
     //     Ok(())
     // }
 
-    // pub fn register_task(ctx: Context<Initialize>) -> ProgramResult {
-    //     Ok(())
-    // }
-
-    // pub fn deregister_task(ctx: Context<Initialize>) -> ProgramResult {
-    //     Ok(())
-    // }
-
+    #[access_control(on_schedule(&ctx))]
     pub fn run_task<'info>(ctx: Context<'_, '_, '_, 'info, RunTask<'info>>) -> ProgramResult {
         let cpi_program = ctx.accounts.task_program.clone();
-        // let cpi_program = ctx.remaining_accounts[0].clone();
-        // let cpi_accounts = ctx.remaining_accounts.to_vec()[1..].to_vec();
         let cpi_accounts = ctx.remaining_accounts.to_vec();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        // TODO: check timing
-
         scheduled::run_scheduled(cpi_ctx)?;
+
+        let spec = &mut ctx.accounts.task_specification;
+        spec.last_executed_ts = ctx.accounts.clock.unix_timestamp;
 
         Ok(())
     }
 }
 
-// #[derive(Accounts)]
-// pub struct Initialize<'info> {}
+pub fn on_schedule<'info>(ctx: &Context<'_, '_, '_, 'info, RunTask<'info>>) -> Result<()> {
+    let spec = &ctx.accounts.task_specification;
+    let current_ts: i64 = ctx.accounts.clock.unix_timestamp;
+    if current_ts < spec.start_ts {
+        msg!(&format!(
+            "current ts: \"{}\" start ts: \"{}\"",
+            current_ts, spec.start_ts
+        ));
+        return Err(ErrorCode::RanEarly.into());
+    }
+    if spec.last_executed_ts != i64::default() {
+        if current_ts < (spec.last_executed_ts + spec.interval_ts) {
+            return Err(ErrorCode::RanOffSchedule.into());
+        }
+    }
+    Ok(())
+}
 
 #[derive(Accounts)]
-pub struct Empty {}
+#[instruction(bump: u8)]
+pub struct RegisterTask<'info> {
+    task_program: AccountInfo<'info>,
 
-#[derive(Accounts)]
-pub struct RegisterTask {}
+    #[account(init, seeds = [task_program.key.as_ref()], bump = bump, payer = registrar)]
+    task_specification: Account<'info, TaskSpecification>,
+
+    #[account(signer)]
+    registrar: AccountInfo<'info>,
+
+    system_program: Program<'info, System>,
+}
 
 // All accounts not included here, i.e., the "remaining accounts" should be task args
 #[derive(Accounts)]
 pub struct RunTask<'info> {
     pub task_program: AccountInfo<'info>,
+
+    #[account(mut, seeds = [task_program.key.as_ref()], bump = task_specification.bump, has_one = task_program)]
+    pub task_specification: Account<'info, TaskSpecification>,
+
     // fee account (PDA?)
-    // signers to transfer from account
+    pub clock: Sysvar<'info, Clock>,
 }
 
-// #[account]
-// pub struct TaskRegistry {
-//     // All registered tasks
-//     accounts: Vec<Task>,
-// }
+#[account]
+#[derive(Default)]
+pub struct TaskSpecification {
+    // Target program to execute
+    task_program: Pubkey,
 
-// #[account]
-// pub struct Task {
-//     // Target program to execute against
-//     program_id: Pubkey,
+    // Schedule info
+    start_ts: i64,
+    interval_ts: i64,
+    last_executed_ts: i64,
 
-//     // Schedule info
-//     min_interval: UnixTimestamp,
-//     last_executed: UnixTimestamp,
-// }
+    // For PDA
+    bump: u8,
+    // TODO: resolver info
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Ran before start time in spec")]
+    RanEarly,
+    #[msg("Ran off schedule")]
+    RanOffSchedule,
+}
--- tests/solcron.ts
+++ tests/solcron.ts
@@ -2,44 +2,119 @@ import * as anchor from "@project-serum/anchor";
 import { strict as assert } from "assert";
 
 describe("sol_cron", () => {
-  // Read the provider from the configured environmnet.
   const provider = anchor.Provider.env();
-
-  // Configure the client to use the provider.
   anchor.setProvider(provider);
 
-  const solCron = anchor.workspace.Solcron;
-  const exampleTask = anchor.workspace.ExampleTask;
+  const solcronProgram = anchor.workspace.Solcron;
+  const counterProgram = anchor.workspace.ExampleTask;
 
   const counter = anchor.web3.Keypair.generate();
   const counterAuthority = anchor.web3.Keypair.generate();
-  const user = provider.wallet.publicKey;
-  let tx;
+  const user = provider.wallet.payer;
+  const userKey = provider.wallet.publicKey;
 
-  it("all", async () => {
-    tx = await exampleTask.rpc.create(counterAuthority.publicKey, {
+  let specAccountKey: anchor.web3.PublicKey, specAccountBump: number;
+
+  it("setup", async () => {
+    await counterProgram.rpc.create(counterAuthority.publicKey, {
       accounts: {
         counter: counter.publicKey,
-        user: user,
+        user: userKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       },
       signers: [counter],
     });
-    console.log("Create tx: ", tx);
 
-    tx = await solCron.rpc.runTask({
+    const counterAccount = await counterProgram.account.counter.fetch(
+      counter.publicKey
+    );
+    assert.ok(counterAccount.authority.equals(counterAuthority.publicKey));
+    assert.ok(counterAccount.count.toNumber() == 0);
+
+    [specAccountKey, specAccountBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [counterProgram.programId.toBuffer()],
+        solcronProgram.programId
+      );
+    console.log("Spec account: ", specAccountKey, specAccountBump);
+  });
+
+  it("register", async () => {
+    const startTs = new anchor.BN(Date.now() / 1000 - 5);
+    const intervalTs = new anchor.BN(startTs.toNumber() + 10);
+
+    await solcronProgram.rpc.registerTask(
+      new anchor.BN(specAccountBump),
+      counterProgram.programId,
+      startTs,
+      intervalTs,
+      {
+        accounts: {
+          taskProgram: counterProgram.programId,
+          taskSpecification: specAccountKey,
+          registrar: userKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [user],
+      }
+    );
+
+    const specAccount = await solcronProgram.account.taskSpecification.fetch(
+      specAccountKey
+    );
+    console.log("Spec account data:\n", specAccount);
+    assert.ok(specAccount.taskProgram.equals(counterProgram.programId));
+    assert.ok(specAccount.startTs.eq(startTs));
+    assert.ok(specAccount.intervalTs.eq(intervalTs));
+    assert.ok(specAccount.lastExecutedTs.toNumber() == 0);
+  });
+
+  it("run", async () => {
+    await solcronProgram.rpc.runTask({
       accounts: {
-        taskProgram: exampleTask.programId,
+        taskProgram: counterProgram.programId,
+        taskSpecification: specAccountKey,
+        clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
       },
       remainingAccounts: [
         { pubkey: counter.publicKey, isWritable: true, isSigner: false },
       ],
     });
-    console.log("Run task tx: ", tx);
 
+    const counterAccount = await counterProgram.account.counter.fetch(
+      counter.publicKey
+    );
+    assert.ok(counterAccount.count.toNumber() == 1);
 
-    const counterAccount = await exampleTask.account.counter.fetch(counter.publicKey)
-    assert.ok(counterAccount.authority.equals(counterAuthority.publicKey))
-    assert.ok(counterAccount.count.toNumber() == 1)
+    const specAccount = await solcronProgram.account.taskSpecification.fetch(
+      specAccountKey
+    );
+    console.log("Spec account data:\n", specAccount);
+    assert.ok(specAccount.lastExecutedTs.toNumber() != 0);
   });
+
+  it("run invalid - program and spec mismatch", async () => {
+    await assert.rejects(
+      async () => {
+        await solcronProgram.rpc.runTask({
+          accounts: {
+            taskProgram: solcronProgram.programId,
+            taskSpecification: specAccountKey,
+            clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+          },
+          remainingAccounts: [
+            { pubkey: counter.publicKey, isWritable: true, isSigner: false },
+          ],
+        });
+      },
+      (err: any) => {
+        assert.equal(err.code, 146);
+        return true;
+      }
+    );
+  });
+
+  it("run early", async () => {});
+
+  it("run off schedule", async () => {});
 });
